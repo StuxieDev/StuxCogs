cc13714de6cee3e71af752e6425805aa5096d021
Stuxie.Dev <contact@stuxie.dev>
2024-06-08 16:21:40 +0100

    Update kitten command

--- animal/animal.py
+++ animal/animal.py
@@ -64,9 +64,10 @@ class Animal(commands.GroupCog):
 
         try:
             async with self.session.get(self.kitten_api) as r:
-                result = await r.json()
+                result = await r.json(content_type='text/html')
         except aiohttp.ClientError as e:
-            await ctx.send(f"Reddit API returned the following error: {e}")
+            await ctx.send(self.error_message)
+            #await ctx.send(f"Reddit API returned the following error: {e}")
         else:
             await ctx.send(result[0]["data"]["children"][0]["data"]["url"])
 
