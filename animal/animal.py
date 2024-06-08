e645457d887631c577f6f84e78d0be38a6e6911c
Stuxie.Dev <contact@stuxie.dev>
2024-06-08 15:36:51 +0100

    Update animal cog

--- animal/animal.py
+++ animal/animal.py
@@ -25,10 +25,10 @@ class Animal(commands.GroupCog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-        self.catapi = "https://api.thecatapi.com/v1/images/search"
-        self.dogapi = "https://dog.ceo/api/breeds/image/random"
-        self.pugapi = "https://dog.ceo/api/breed/pug/images/random"
-        self.foxapi = "http://wohlsoft.ru/images/foxybot/randomfox.php"
+        self.cat_api = "https://api.thecatapi.com/v1/images/search"
+        self.dog_api = "https://dog.ceo/api/breeds/image/random"
+        self.pug_api = "https://dog.ceo/api/breed/pug/images/random"
+        self.fox_api = "http://wohlsoft.ru/images/foxybot/randomfox.php"
         self.dog_breed_api = "https://dog.ceo/api/breed/{}/images/random"
         self.error_message = "An API error occured. Probably just a hiccup.\nIf this error persist for several days, please report it."
 
@@ -39,11 +39,13 @@ class Animal(commands.GroupCog):
     @commands.cooldown(1, 60, commands.BucketType.guild)
     async def cat(self, ctx):
         """Shows a cat"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         try:
-            async with self.session.get(self.catapi) as r:
+            async with self.session.get(self.cat_api) as r:
                 result = await r.json()
         except aiohttp.ClientError:
             await ctx.send(self.error_message)
@@ -56,15 +58,19 @@ class Animal(commands.GroupCog):
         """Throws a cat bomb!
 
         Defaults to 5, max is 10"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         results = []
+
         if amount > 10 or amount < 1:
             amount = 5
+
         for x in range(0, amount):
             try:
-                async with self.session.get(self.catapi) as r:
+                async with self.session.get(self.cat_api) as r:
                     api_result = await r.json()
             except aiohttp.ClientError:
                 await ctx.send(self.error_message)
@@ -76,11 +82,14 @@ class Animal(commands.GroupCog):
                 await ctx.send(self.error_message)
                 return
 
+        embed_pages = []
+
         for i in results:
             embed = discord.Embed(color=await ctx.embed_colour())
             embed.set_image(url=i)
             embed.set_footer(text=f"Page {results.index(i)+1}/{len(results)}")
             embed_pages.append(embed)
+
         await menu(ctx, embed_pages)
 
     @commands.hybrid_command()
@@ -94,24 +103,29 @@ class Animal(commands.GroupCog):
         You can also provide "list" for a list of all 
         available breeds.
         """
+
         if breed.lower() == "list":
             if not await ctx.embed_requested():
                 await ctx.send("I need to be able to send embeds to show the list.")
                 return
+
             try:
                 async with self.session.get("https://dog.ceo/api/breeds/list/all") as r:
                     result = await r.json()
             except aiohttp.ClientError:
                 await ctx.send(self.error_message)
                 return
+
             try:
                 result = result["message"]
             except (TypeError, KeyError):
                 await ctx.send(self.error_message)
                 return
+
             breed_list = [i for i, _ in filter(lambda x: not bool(x[-1]), list(result.items()))]
             embed_pages = []
             c = list(chunks(breed_list, 10))
+
             for page in c:
                 embed = discord.Embed(
                     title="Breeds list",
@@ -120,22 +134,27 @@ class Animal(commands.GroupCog):
                 )
                 embed.set_footer(text=f"Page {c.index(page)+1}/{len(c)}")
                 embed_pages.append(embed)
+
             await menu(ctx, embed_pages)
             return
+
         else: 
             if not await ctx.embed_requested():
                 await ctx.send("I need to be able to send embeds for this command.")
                 return
+
             if breed.lower() == "random":
-                api = self.dogapi
+                api = self.dog_api
             else:
                 api = self.dog_breed_api.format(breed)
+
             try:
                 async with self.session.get(api) as r:
                     result = await r.json()
             except aiohttp.ClientError:
                 await ctx.send(self.error_message)
                 return
+
             try:
                 msg = result['message']
             except (TypeError, KeyError):
@@ -156,16 +175,20 @@ class Animal(commands.GroupCog):
         You can also provide "list" for a list of all 
         available breeds.
         """
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         if breed.lower() == "random":
-            api = self.dogapi
+            api = self.dog_api
         else:
             api = self.dog_breed_api.format(breed)
+
         results = []
         if amount > 10 or amount < 1:
             amount = 5
+
         for x in range(0,amount):
             try:
                 async with self.session.get(api) as r:
@@ -179,26 +202,31 @@ class Animal(commands.GroupCog):
                 return await ctx.send(self.error_message)
 
         embed_pages = []
+
         for i in results:
             embed = discord.Embed(color=await ctx.embed_colour())
             embed.set_image(url=i)
             embed.set_footer(text=f"Page {results.index(i)+1}/{len(results)}")
             embed_pages.append(embed)
+
         await menu(ctx, embed_pages)
 
     @commands.hybrid_command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
     async def fox(self, ctx):
         """Shows a fox"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         try:
-            async with self.session.get(self.foxapi) as r:
+            async with self.session.get(self.fox_api) as r:
                 result = await r.json()
         except aiohttp.ClientError:
             await ctx.send(self.error_message)
             return
+
         try:
             file = result['file']
         except (TypeError, KeyError):
@@ -212,15 +240,19 @@ class Animal(commands.GroupCog):
         """Throws a fox bomb!
 
         Defaults to 5, max is 10"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         results = []
+
         if amount > 10 or amount < 1:
             amount = 5
+
         for x in range(0, amount):
             try:
-                async with self.session.get(self.foxapi) as r:
+                async with self.session.get(self.fox_api) as r:
                     api_result = await r.json()
             except aiohttp.ClientError:
                 await ctx.send(self.error_message)
@@ -232,26 +264,32 @@ class Animal(commands.GroupCog):
                 await ctx.send(self.error_message)
                 return
 
+        embed_pages = []
+
         for i in results:
             embed = discord.Embed(color=await ctx.embed_colour())
             embed.set_image(url=i)
             embed.set_footer(text=f"Page {results.index(i)+1}/{len(results)}")
             embed_pages.append(embed)
+
         await menu(ctx, embed_pages)
 
     @commands.hybrid_command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
     async def pug(self, ctx):
         """Shows a pug"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         try:
-            async with self.session.get(self.pugapi) as r:
+            async with self.session.get(self.pug_api) as r:
                 result = await r.json()
         except aiohttp.ClientError:
             await ctx.send(self.error_message)
             return
+
         try:
             file = result['message']
         except (TypeError, KeyError):
@@ -265,15 +303,19 @@ class Animal(commands.GroupCog):
         """Throws a pugs bomb!
 
         Defaults to 5, max is 10"""
+
         if not await ctx.embed_requested():
             await ctx.send("I need to be able to send embeds for this command.")
             return
+
         results = []
+
         if amount > 10 or amount < 1:
             amount = 5
+
         for x in range(0,amount):
             try:
-                async with self.session.get(self.pugapi) as r:
+                async with self.session.get(self.pug_api) as r:
                     api_result = await r.json()
             except aiohttp.ClientError:
                 await ctx.send(self.error_message)
@@ -285,9 +327,12 @@ class Animal(commands.GroupCog):
                 await ctx.send(self.error_message)
                 return
 
+        embed_pages = []
+
         for i in results:
             embed = discord.Embed(color=await ctx.embed_colour())
             embed.set_image(url=i)
             embed.set_footer(text=f"Page {results.index(i)+1}/{len(results)}")
             embed_pages.append(embed)
+
         await menu(ctx, embed_pages)
